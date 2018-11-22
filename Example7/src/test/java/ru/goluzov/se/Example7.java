8abac21ffc6fd3476bfde82cee75ea1a54039137
Alecs <goluzov_a@mail.ru>
2018-11-22 21:48:17 +0300

    HomeWork7

---
+++ Example7/src/test/java/ru/goluzov/se/Example7.java
@@ -0,0 +1,19 @@
+package ru.goluzov.se;
+
+public class Example7 {
+    public static void main(String[] args) {
+        Cat[] cats = {
+                new Cat("Barsik", 10),
+                new Cat("Mirka", 15),
+                new Cat("Belok", 25),
+                new Cat("Tuzik", 5),
+                new Cat("Cat", 1)};
+        Plate plate = new Plate(30);
+        for (Cat cat : cats) {
+            cat.eat(plate);
+            cat.info();
+        }
+        plate.addFood(50);
+        plate.info();
+    }
+}
